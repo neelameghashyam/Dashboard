f2b4dfaf42453de382aff1cdde98792c47c6f228
Shyam <neelameghashyam@gmail.com>
2025-04-23 21:19:16 +0530

    stepper added

--- src/app/pages/analytics/analytics.component.ts
+++ src/app/pages/analytics/analytics.component.ts
@@ -1,11 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DarkModeService } from 'src/app/services/dark-theme/dark-mode.service';
+import { MatStepperModule, MatStepper } from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatOptionModule } from '@angular/material/core';
+import { MatError } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-analytics',
   templateUrl: './analytics.component.html',
-  styleUrl: './analytics.component.scss'
+  styleUrls: ['./analytics.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    MatIconModule,
+    MatOptionModule,
+    MatError
+  ]
 })
 export class AnalyticsComponent {
+  @ViewChild('stepper') stepper!: MatStepper;
+  isLinear = true;
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+  thirdFormGroup: FormGroup;
 
+  constructor(
+    private _formBuilder: FormBuilder,
+    public darkMode: DarkModeService
+  ) {
+    this.firstFormGroup = this._formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required]
+    });
+
+    this.secondFormGroup = this._formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.pattern(/^[0-9]{10}$/)]]
+    });
+
+    this.thirdFormGroup = this._formBuilder.group({
+      address: [''],
+      city: ['']
+    });
+  }
+
+  submitForm() {
+    if (this.firstFormGroup.valid && this.secondFormGroup.valid) {
+      console.log('Form submitted:', {
+        personalInfo: this.firstFormGroup.value,
+        contactInfo: this.secondFormGroup.value,
+        addressInfo: this.thirdFormGroup.value
+      });
+  
+      // Reset all form groups
+      this.firstFormGroup.reset();
+      this.secondFormGroup.reset();
+      this.thirdFormGroup.reset();
+  
+      // Reset the stepper to the first step
+      this.stepper.reset();
+    }
+  }
 }
